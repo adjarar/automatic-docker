cbf52047d1e50fb4c3baacd0c6dc8c2f916770f1
adjarar <82608399+adjarar@users.noreply.github.com>
2023-05-11 06:23:14 +0200

    Update onstart.sh

--- onstart.sh
+++ onstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/adjarar/automatic-user-files/main/load_models.sh
-chmod +x on_docker_start.sh
+chmod +x load_models.sh.sh
 mv load_models.sh /home/webui
 
 # Switch to the webui user
